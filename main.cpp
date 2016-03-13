e2902a1ea047f2496679422f11b768a6ed41438e
onyxOmega <william.fischer42@gmail.com>
2016-03-13 08:37:47 -0600

    C1
    Libraries Added

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <ostream>
+#include <fstream>
+#include <sstream>
+#include <boost/lexical_cast.hpp>
 
 using namespace std;
 
